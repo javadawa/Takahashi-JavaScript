b7096330e4cb99f616f6f1e666ed1e0b58ffa447
takataka <javadava.58.pts@gmail.com>
2020-11-05 23:51:32 +0900

    first commit

---
+++ EVENT/js/event.js
@@ -0,0 +1,10 @@
+'use strict';
+function welcome() {
+    alert('Welcome!');
+}
+function girl() {
+    window.open("img/dillon-kydd-9NLYjXY4r4k-unsplash.jpg", "sample", "width=200", "height=200");
+}
+function atNight() {
+    window.open("img/andy-holmes-xA26xebY3dw-unsplash.jpg", "sample", "width=200", "height=200");
+}
